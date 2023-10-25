9ed283175f84c37e81795fc3397fba6aea7b3164
Andreas Vasiliou <andreas.vasiliou14@outlook.com>
2023-10-25 17:22:06 +0300

    create login command

---
+++ src/Volt.Application/Features/Account/Commands/LoginCommand.cs
@@ -0,0 +1,29 @@
+﻿using Volt.Application.Services;
+
+namespace Volt.Application.Features.Account.Commands;
+
+public record LoginRequest(string Usename, string Password) : IRequest<AuthenticationResult>;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Usename).NotEmpty().WithMessage("Username is required");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+    }
+}
+
+public class LoginCommand : IRequestHandler<LoginRequest, AuthenticationResult>
+{
+    private readonly IIdentityService _identityService;
+
+    public LoginCommand(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<AuthenticationResult> Handle(LoginRequest request, CancellationToken cancellationToken)
+    {
+        return await _identityService.LoginAsync(request.Usename, request.Password);
+    }
+}
